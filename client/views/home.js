f86103c532d99be1160d2abff867634e95007885
pahans <pahan123@gmail.com>
2014-02-09 20:42:51 +0530

    map search box

--- client/views/home.js
+++ client/views/home.js
@@ -1,15 +1,63 @@
-Template.home.rendered = function() {  
-  GoogleMaps.init(
-      {
-          'sensor': true, //optional
-      }, 
-      function(){
-          var mapOptions = {
-              zoom: 13,
-              // mapTypeId: google.maps.MapTypeId.SATELLITE
+Template.home.rendered = function() {
+  GoogleMaps.init({
+      'sensor': true, //optional
+      'libraries': 'places'
+    },
+    function() {
+      var mapOptions = {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+      map.setCenter(new google.maps.LatLng(6.927078600000000000, 79.861243000000060000));
+      var input = (document.getElementById('pac-input'));
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+      var searchBox = new google.maps.places.SearchBox((input));
+      var markers = [];
+      google.maps.event.addListener(searchBox, 'places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        for (var i = 0, marker; marker = markers[i]; i++) {
+          marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+          var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
           };
-          map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
-          map.setCenter(new google.maps.LatLng( 6.927078600000000000, 79.861243000000060000 ));
-      }
+
+          // Create a marker for each place.
+          var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+          });
+
+          markers.push(marker);
+
+          bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+      });
+
+
+      google.maps.event.addListener(map, 'bounds_changed', function() {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+      });
+
+    }
   );
 };
